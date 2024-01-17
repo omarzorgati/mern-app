465710ab3473b48869e552e87b88b4f6d60ca00d
omarzorgati <Omar.ZORGATI@polytechnicien.tn>
2024-01-17 18:21:59 +0100

    creating the database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 
 
 const app = express();
+app.use(express.json());
 
 
 app.listen(3007, () => { 
     console.log('Server is running on port 3007');
 });
+mongoose.connect(process.env.MONGO).then (()=>{
+    console.log('Database connected');
+}) .catch((err)=>{
+    console.log(err);
+})
+
