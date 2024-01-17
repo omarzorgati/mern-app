c861b3764dd59a1e75fe05417e986bddfe3daac6
omarzorgati <Omar.ZORGATI@polytechnicien.tn>
2024-01-17 15:40:41 +0100

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
 
 export default function App() {
-  
+
   return (
-    <div className='text-6xl'>
-      aaaaaa
-    </div>
+ <BrowserRouter>
+ <Header/>
+ <Routes>
+  <Route path="/" element={<Home/>} />
+  <Route path="/projects" element={<Projects/>} />
+  <Route path="/about" element={<About/>} />
+  <Route path="/sign-in" element={<SignIn/>} />
+  <Route path="/sign-up" element={<SignUp/>} />
+  <Route path="/dashboard" element={<Dashboard/>} />
+
+
+
+ </Routes>
+ </BrowserRouter>
   )
 }
