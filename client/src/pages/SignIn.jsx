bdb270f6c0136427811a6c3f40d4661b0e7a9e40
omarzorgati <Omar.ZORGATI@polytechnicien.tn>
2024-01-18 18:49:08 +0100

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,17 @@
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/user.slice.js';
 
 
 export default function SignIn() {
 const navigate=useNavigate();
 const[formData,setFormData]=useState({});
-const[loading,setLoading]=useState(false);
-const[error,setError]=useState(null);
+const{loading,error}=useSelector((state)=>state.user)
+const dispatch = useDispatch();
 //trim: remove the spaces at the beginning and at the end of the string
 const handleChange=(e)=>{
   setFormData({
@@ -20,11 +23,10 @@ const handleChange=(e)=>{
 const handleSubmit=async(e)=>{
   e.preventDefault();
   if(!formData.email || !formData.password){
-      return setError('All fields are required');
+      return dispatch(signInFailure('please fill all the fields'));
   }
   try {
-    setLoading(true);
-    setError(null);
+    dispatch(signInStart());
     const res = await fetch('api/auth/signin',{
       method:'POST',
       headers:{
@@ -34,16 +36,13 @@ const handleSubmit=async(e)=>{
     });
     const data=await res.json();
     if(data.success === false){
-      setError(data.message)
-      setLoading(false);
+      dispatch(signInFailure(data.message));
       return ;
       }
-    setLoading(false);
-    setError(null);
+    dispatch(signInSuccess(data));
     navigate('/');
   } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
   }
 
 };
@@ -78,7 +77,7 @@ const handleSubmit=async(e)=>{
                 <Spinner size='sm'/>
                 <span>loading...</span>
                 </>
-                ) : 'Sign Up'
+                ) : 'Sign In'
                
               }
             </Button>
---
+++ client/src/redux/user/user.slice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+//initialisation
+const initialState = {
+    //before the login
+    currentUser: null,
+    error:null,
+    loading:false,
+};
+// slice :managing a specific piece of the application state and includes the reducer function and action creators.
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        signInSuccess: (state, action) => {
+            state.loading = false;
+            // this is the data we get from the backend as a response
+            state.currentUser = action.payload;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+    },
+  }
+})
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export default userSlice.reducer;
