e30456c870dfdc3726b2b930e0009be60e5e5257
omarzorgati <Omar.ZORGATI@polytechnicien.tn>
2024-01-18 13:06:18 +0100

    create an error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utilities/error.js";
 
 
 
-export const signup = async(req,res)=>{
+export const signup = async(req,res,next)=>{
     const {username,email,password} = req.body;
 
     if(!username || !email || !password || username ==='' || email === ''|| password===''){
-        return res.status(422).json({message:"Please add all the fields"});
+        next(errorHandler(400,"All fields are required"));
     }
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
@@ -15,7 +16,7 @@ export const signup = async(req,res)=>{
         await newUser.save();
         res.status(201).json({message:"User created successfully"});
     } catch (error) {
-        res.status(500).json({message:error.message});
+        next(error);
     }
     
 };
--- api/index.js
+++ api/index.js
@@ -28,3 +28,14 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
 
+app.use((err, req, res, next) => {
+    const statusCode= err.statusCode || 500; //500:internal server error
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
+
